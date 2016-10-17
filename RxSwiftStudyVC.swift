50d4c8aad311a42bbda028477b9b8219f3fa3902
Grubby <goreyjp@qq.com>
2016-10-17 13:40:47 +0800

    TableViewCell 可以正常显示

--- RxSwiftStudyVC.swift
+++ RxSwiftStudyVC.swift
@@ -22,6 +22,9 @@ class RxSwiftStudyVC : UITableViewController {
         view.backgroundColor = UIColor.white
         title = "Gank"
         
+        tableView.register(MeiziCell.self, forCellReuseIdentifier: "cell")
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+
         loadData()
         
     }
@@ -49,7 +52,6 @@ class RxSwiftStudyVC : UITableViewController {
             })
             .subscribeNext({ (json) in
                 if let results = json["results"].array {
-                    
                     self.demos.removeAll()
                     results.forEach({ (line) in
                         self.demos.append(line)
@@ -69,16 +71,68 @@ class RxSwiftStudyVC : UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? MeiziCell ?? MeiziCell(style: .default, reuseIdentifier: "cell")
+        
         let j = demos[indexPath.row]
-        let desc = j["desc"].string ?? "-"
-        let img = j["url"].string ?? ""
-        cell.textLabel?.text = desc
-        cell.imageView?.kf.setImage(with: URL(string: img))
+        cell.updateWith(j: j)
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
+    }
+}
+
+
+class MeiziCell : UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+        contentView.addSubview(ivAvatar)
+        ivAvatar.snp.makeConstraints { (m) in
+            m.left.equalTo(contentView)
+            m.top.equalTo(contentView).offset(5)
+            m.right.equalTo(contentView)
+            m.height.equalTo(400)
+        }
+        
+        contentView.addSubview(lbDesc)
+        lbDesc.snp.makeConstraints { (m) in
+            m.left.equalTo(contentView).offset(5)
+            m.top.equalTo(self.ivAvatar.snp.bottom).offset(5)
+            m.right.equalTo(contentView).offset(5)
+            m.bottom.equalTo(contentView).offset(-5)
+        }
+        
+        lbDesc.preferredMaxLayoutWidth = UIScreen.main.bounds.size.width - 10
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var ivAvatar : UIImageView = {
+        let a = UIImageView()
+        a.contentMode = .scaleAspectFill
+        a.clipsToBounds = true
+        return a
+    }()
+    
+    lazy var lbDesc : UILabel = {
+        let a = UILabel()
+        a.numberOfLines = 0
+        return a
+    }()
+    
+    func updateWith(j:JSON) {
+        let desc = j["desc"].string ?? "-"
+        let img = j["url"].string ?? ""
+        lbDesc.text = desc
+        ivAvatar.kf.setImage(with: URL(string: img))
     }
 }
